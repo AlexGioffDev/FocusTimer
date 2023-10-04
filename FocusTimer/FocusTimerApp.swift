e9883979fa343228c949d85a32eaf2feb7f2e4d6
Alex Gioffre <alex.gioffre_dev@outlook.it>
2023-10-04 17:59:26 +0200

    Initial Commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by Alex Gioffre' on 04/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
